09eb1f1b6b99160b02137866b4fc656ce25c232a
mushfiqur-rahman <mushfiq.style@gmail.com>
2022-03-20 17:38:32 +0600

    fist-commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $(".project-popup").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+
+  $(".video").magnificPopup({
+    type: "iframe"
+  });
+
+  $('.counter').counterUp();
+
+  $('.testimonials').slick({
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
+    dots: true,
+  });
+});
+
+$(window).on('scroll', function () {
+  var scroll = $(window).scrollTop();
+  if (scroll < 10) {
+    $("#sticky-header").removeClass("sticky");
+  } else {
+    $("#sticky-header").addClass("sticky");
+  }
+});
